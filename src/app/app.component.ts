698e1e25bc8804bb8fa007a791338f095b739e3b
Brian Fan <kuomu.fan@yahoo.com>
2017-06-09 08:39:07 -0700

    create animal model and hardcoded few animal data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  animalList: Animal[] = [{
+      species: "Arctic Fox",
+      name: "Moon",
+      age: 2,
+      diet: "Carnivore",
+      location: "Northern Trail",
+      caretakers: 5,
+      sex: "Female",
+      likes: "Cool shade",
+      dislikes: "Loud noises"
+    },{
+      species: "Ocelot",
+      name: "Prince",
+      age: 4,
+      diet: "Carnivore",
+      location: "Tropical Rain Forest Building",
+      caretakers: 6,
+      sex: "Male",
+      likes: "Laying in the sunshine",
+      dislikes: "Toys that are not rope-based"
+    },{
+      species: "Northwest Black Tailed Deer",
+      name: "Tinkerbell",
+      age: 8,
+      diet: "Herbivore",
+      location: "Northern Trail",
+      caretakers: 2,
+      sex: "Female",
+      likes: "Delicate roots and leaves",
+      dislikes: "Loud Noises"
+    }
+  ];
 }
